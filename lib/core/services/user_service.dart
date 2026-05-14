5111312282e7d4c3d9c44e63c1356fbc64b44947
Naseem <muhammednaseemtk000@gmail.com>
2026-05-14 12:19:12 +0530

    some changes for this app

--- lib/core/services/user_service.dart
+++ lib/core/services/user_service.dart
@@ -1,10 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../../views/home/models/user_model.dart';
 
 class UserService {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final FirebaseAuth auth = FirebaseAuth.instance;
 
-  CollectionReference get usersCollection => firestore.collection('users');
+  String get currentUserId {
+    final user = auth.currentUser;
+
+    if (user == null) {
+      throw Exception('User not logged in');
+    }
+
+    return user.uid;
+  }
+
+  CollectionReference get usersCollection =>
+      firestore.collection('users').doc(currentUserId).collection('people');
 
   Future<List<UserModel>> getUsers({int page = 0, int pageSize = 20}) async {
     final QuerySnapshot querySnapshot = await usersCollection
@@ -13,25 +26,43 @@ class UserService {
         .get();
 
     final List<QueryDocumentSnapshot> docs = querySnapshot.docs;
-    final startIndex = page * pageSize;
-    if (startIndex >= docs.length) return [];
 
-    final endIndex = (startIndex + pageSize > docs.length) ? docs.length : startIndex + pageSize;
+    final startIndex = page * pageSize;
+
+    if (startIndex >= docs.length) {
+      return [];
+    }
+
+    final endIndex = (startIndex + pageSize > docs.length)
+        ? docs.length
+        : startIndex + pageSize;
+
     final pageDocs = docs.sublist(startIndex, endIndex);
 
-    return pageDocs
-        .map((doc) => UserModel.fromJson({...doc.data() as Map<String, dynamic>, 'id': doc.id}))
-        .toList();
+    return pageDocs.map((doc) {
+      return UserModel.fromJson({
+        ...doc.data() as Map<String, dynamic>,
+        'id': doc.id,
+      });
+    }).toList();
   }
 
   Future<UserModel?> getUserById(String id) async {
     final DocumentSnapshot doc = await usersCollection.doc(id).get();
-    if (!doc.exists) return null;
-    return UserModel.fromJson({...doc.data() as Map<String, dynamic>, 'id': doc.id});
+
+    if (!doc.exists) {
+      return null;
+    }
+
+    return UserModel.fromJson({
+      ...doc.data() as Map<String, dynamic>,
+      'id': doc.id,
+    });
   }
 
   Future<UserModel> addUser(UserModel user) async {
     final DocumentReference docRef = await usersCollection.add(user.toJson());
+
     return UserModel(
       id: docRef.id,
       name: user.name,
@@ -44,6 +75,7 @@ class UserService {
 
   Future<UserModel> updateUser(UserModel user) async {
     await usersCollection.doc(user.id).update(user.toJson());
+
     return user;
   }
 
@@ -52,15 +84,22 @@ class UserService {
   }
 
   Stream<List<UserModel>> watchUsers() {
-    return usersCollection.orderBy('createdAt', descending: true).snapshots().map(
-          (snapshot) => snapshot.docs
-              .map((doc) => UserModel.fromJson({...doc.data() as Map<String, dynamic>, 'id': doc.id}))
-              .toList(),
+    return usersCollection
+        .orderBy('createdAt', descending: true)
+        .snapshots()
+        .map(
+          (snapshot) => snapshot.docs.map((doc) {
+            return UserModel.fromJson({
+              ...doc.data() as Map<String, dynamic>,
+              'id': doc.id,
+            });
+          }).toList(),
         );
   }
 
   Future<int> getUserCount() async {
     final AggregateQuerySnapshot snapshot = await usersCollection.count().get();
+
     return snapshot.count ?? 0;
   }
 }
